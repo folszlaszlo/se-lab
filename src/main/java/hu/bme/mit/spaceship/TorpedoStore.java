4da359bdbb15372fa056a13614764bd754b2a5ed
Fölsz László <231101016+folszlaszlo@users.noreply.github.com>
2025-10-15 17:46:54 +0200

    a

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -18,7 +18,7 @@ public class TorpedoStore {
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
-    // update failure rate if it was specified in an environment variable
+    // update failure rate if it was specified in an environment variablea
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null){
       try {
